0ff197a6e1ca9c0220cfba57ea4dc282283bb2b2
luisfalconmx <luisfalconmx@gmail.com>
2024-01-04 00:18:50 -0600

    graphql mesh configured and used with github source

--- src/config/codegen.ts
+++ src/config/codegen.ts
@@ -1,21 +1,21 @@
-import { CodegenConfig } from '@graphql-codegen/cli'
-import { githubConfig, hashnodeConfig } from '.'
+// import { CodegenConfig } from '@graphql-codegen/cli'
+// import { githubConfig, hashnodeConfig } from '.'
 
-const config: CodegenConfig = {
-  generates: {
-    './src/generated/hashnode.schema.ts': {
-      schema: [hashnodeConfig.endpoint],
-      documents: ['../**/*.graphql'],
-      plugins: [
-        'typescript',
-        'typescript-operations',
-        'typescript-react-apollo'
-      ]
-    }
-  },
-  hooks: {
-    afterAllFileWrite: ['prettier --write']
-  }
-}
+// const config: CodegenConfig = {
+//   generates: {
+//     './src/generated/hashnode.schema.ts': {
+//       schema: ['http://localhost:3000/api/graphql'],
+//       documents: ['../**/*.graphql'],
+//       plugins: [
+//         'typescript',
+//         'typescript-operations',
+//         'typescript-react-apollo'
+//       ]
+//     }
+//   },
+//   hooks: {
+//     afterAllFileWrite: ['prettier --write']
+//   }
+// }
 
-export default config
+// export default config
