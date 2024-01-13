03c9e13d265330010c07ffbbc24bf074ea8627f8
luisfalconmx <luisfalconmx@gmail.com>
2024-01-13 11:03:53 -0600

    feat: carousel component created

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,40 @@
+import { Splide, SplideTrack } from '@splidejs/react-splide'
+import { ChevronRight } from 'react-feather'
+import styles from './Carousel.module.css'
+import type { ReactNode } from 'react'
+import type { Options } from '@splidejs/react-splide'
+
+interface Props {
+  children: ReactNode
+  className?: string
+}
+
+const Carousel = ({ children, className = '' }: Props) => {
+  const carouselOptions: Options = {
+    pagination: false,
+    rewind: true,
+    arrows: true,
+    keyboard: true,
+    autoplay: true
+  }
+
+  return (
+    <Splide
+      aria-label="My Favorite Images"
+      options={carouselOptions}
+      hasTrack={false}
+      className={`${styles.Carousel} ${className}`}
+    >
+      <SplideTrack>{children}</SplideTrack>
+
+      <div className="splide__arrows">
+        <button className="splide__arrow splide__arrow--prev !hidden"></button>
+        <button className={styles.Carousel__arrow}>
+          <ChevronRight size={62} color="#ffffff" />
+        </button>
+      </div>
+    </Splide>
+  )
+}
+
+export default Carousel
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,14 +1,14 @@
 import hashnodeClient from '@/clients/hashnodeClient'
 import CardPost from '@/components/CardPost'
+import Carousel from '@/components/Carousel'
 import { HASHNODE_HOST } from '@/config'
 import {
   LatestBlogPostsDocument,
   LatestBlogPostsQuery,
   LatestBlogPostsQueryVariables
 } from '@/generated/hashnode.schema'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
+import { SplideSlide } from '@splidejs/react-splide'
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
-import type { Options } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
 
 export const getStaticProps = (async () => {
@@ -35,23 +35,14 @@ export const getStaticProps = (async () => {
 export default function Blog({
   posts
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const carouselOptions: Options = {
-    pagination: false,
-    rewind: true,
-    arrows: false,
-    keyboard: true,
-    autoplay: true
-  }
-
   return (
     <main className="mx-auto my-24 max-w-[1200px]">
-      <Splide aria-label="My Favorite Images" options={carouselOptions}>
+      <Carousel className="mb-16">
         {posts.slice(0, 3).map((i) => (
           <SplideSlide key={i.node.id}>
             <CardPost
               variant="jumbo"
-              className="mb-24"
-              key={i.node.id}
+              className=""
               title={i.node.title}
               description={i.node.brief || ''}
               image={i.node.coverImage?.url || ''}
@@ -71,7 +62,7 @@ export default function Blog({
             />
           </SplideSlide>
         ))}
-      </Splide>
+      </Carousel>
 
       <div className="mb-16 grid grid-cols-3 gap-4">
         {posts.slice(3, 9).map((i) => (
