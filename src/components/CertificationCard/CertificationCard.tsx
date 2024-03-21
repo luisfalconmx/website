ee2aef5e9a70a0109cd85ff2d0d99aa38ba71671
luisfalconmx <luisfalconmx@gmail.com>
2024-03-21 17:39:28 -0600

    fix: solving minor accessibility issues

--- src/components/CertificationCard/CertificationCard.tsx
+++ src/components/CertificationCard/CertificationCard.tsx
@@ -17,25 +17,34 @@ const CertificationCard = ({
   return (
     <Card isFooterBlurred className="h-[430px] w-full">
       <Image
+        width={600}
+        height={430}
         alt={name}
         src={image}
         removeWrapper
-        className="z-0 h-full w-full object-cover"
+        className="z-0 h-auto w-auto object-cover"
       />
       <CardFooter className="absolute bottom-0 z-10 min-h-[90px] border-t-1 border-default-600 bg-black/70 dark:border-default-100">
         <div className="flex flex-grow items-center gap-2">
           <Image
             alt={organizationName}
             src={organizationImage}
-            className="h-12 w-12 rounded-full bg-black"
+            width={48}
+            height={48}
+            className="aspect-square h-auto w-auto rounded-full bg-black"
           />
           <div className="flex flex-col">
             <p className="text-md font-bold text-white">{name}</p>
             <p className="text-small text-white">{renderDate}</p>
           </div>
         </div>
-        <Link href={credentialUrl}>
-          <Button size="sm" variant="light">
+        <Link href={credentialUrl} aria-label={`View certification of ${name}`}>
+          <Button
+            size="sm"
+            variant="light"
+            id={name}
+            title="View certification details"
+          >
             <ArrowTopRightOnSquareIcon className="h-7 w-7" />
           </Button>
         </Link>
--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -6,7 +6,13 @@ const SkillCard = ({ name, icon }: SkillCardProps) => {
   return (
     <Card isHoverable className="border border-divider bg-transparent">
       <CardBody className="grid grid-cols-[32px_1fr] items-center gap-x-5">
-        <Image src={icon} alt={`${name} icon`} width={32} height={32} />
+        <Image
+          src={icon}
+          alt={`${name} icon`}
+          width={32}
+          height={32}
+          className="aspect-square h-auto w-auto"
+        />
         <p className="text-lg">{name}</p>
       </CardBody>
     </Card>
