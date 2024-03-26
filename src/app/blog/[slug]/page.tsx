48b91034d1ed46726895a74b351946783095f26b
luisfalconmx <luisfalconmx@gmail.com>
2024-03-26 16:07:43 -0600

    perf: accesibility improvements and image priority changes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -39,11 +39,12 @@ export default async function Post({ params }: { params: { slug: string } }) {
           width={1000}
           height={400}
           className="mb-6"
+          priority={false}
         />
         <div className="px-4 lg:px-12">
           <time
             dateTime={post.published_at}
-            className="mb-6 block w-fit rounded-xl border border-divider-soft px-3 py-2 text-sm font-medium text-black/50 dark:border-divider-hard dark:text-white/50"
+            className="mb-6 block w-fit rounded-xl border border-divider-soft px-3 py-2 text-sm font-medium text-black/75 dark:border-divider-hard dark:text-white/75"
           >
             {humanDate(post.published_at)}
           </time>
@@ -62,18 +63,19 @@ export default async function Post({ params }: { params: { slug: string } }) {
             width={90}
             height={90}
             className="mx-auto mb-4 block rounded-full"
+            priority={false}
           />
           <div className="text-center">
             <h2 className="text-lg font-bold">{post.user.name}</h2>
             <Link
               href={post.user.website_url}
               rel="author"
-              className="mb-4 block text-black/50 underline dark:text-white/50"
+              className="mb-4 block text-black/75 underline dark:text-white/75"
             >
               @{post.user.username}
             </Link>
 
-            <p className="mb-6 text-base text-night/50 dark:text-white/50">
+            <p className="mb-6 text-base text-night/50 dark:text-white/75">
               I am frontend developer with 4 years of experience, B1 English
               level and more than 100 certifications related to software
               development.
@@ -81,29 +83,40 @@ export default async function Post({ params }: { params: { slug: string } }) {
 
             <ul className="mx-auto flex w-fit items-center space-x-4">
               <li>
-                <Link href={post.user.website_url}>
+                <Link
+                  href={post.user.website_url}
+                  aria-label={`go to ${post.user.username} official website`}
+                >
                   <GlobeAmericasIcon className="h-8 w-8 text-black dark:text-white" />
                 </Link>
               </li>
               <li>
-                <Link href={`https://github.com/${post.user.github_username}`}>
+                <Link
+                  href={`https://github.com/${post.user.github_username}`}
+                  aria-label={`go to ${post.user.username} github profile`}
+                >
                   <Image
                     src={GithubIcon}
                     alt={`${post.user.name}'s GitHub profile`}
                     width={28}
                     height={28}
                     className="dark:invert"
+                    priority={false}
                   />
                 </Link>
               </li>
               <li>
-                <Link href={`https://twitter.com/${post.user.github_username}`}>
+                <Link
+                  href={`https://twitter.com/${post.user.twitter_username}`}
+                  aria-label={`go to ${post.user.profile} twitter name`}
+                >
                   <Image
                     src={TwitterIcon}
                     alt={`${post.user.name}'s Twitter profile`}
                     width={26}
                     height={26}
                     className="dark:invert"
+                    priority={false}
                   />
                 </Link>
               </li>
@@ -118,7 +131,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
               {post.tags.map((tag: string) => (
                 <li
                   key={tag}
-                  className="rounded-full border border-divider-soft px-3 py-1 text-sm text-black/50 dark:border-divider-hard dark:text-white/50"
+                  className="rounded-full border border-divider-soft px-3 py-1 text-sm text-black/75 dark:border-divider-hard dark:text-white/75"
                 >
                   {tag}
                 </li>
